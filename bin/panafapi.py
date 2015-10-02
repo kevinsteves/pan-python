08561cc81b7a070444e0f0a9b8d01642353e5f7f
Kevin Steves <kevin.steves@pobox.com>
2015-10-02 15:55:22 -0700

    Handle tags response format for field total_count and tags list length in status line.

--- bin/panafapi.py
+++ bin/panafapi.py
@@ -300,22 +300,32 @@ def print_status(afapi, action, exception_msg=None):
             if 'af_complete_percentage' in afapi.json:
                 print(' %s%%' % afapi.json['af_complete_percentage'],
                       end='', file=sys.stderr)
+
             if 'hits' in afapi.json:
                 if 'hits' in afapi.json['hits']:
                     hits = len(afapi.json['hits']['hits'])
                 else:
                     hits = len(afapi.json['hits'])
                 print(' hits=%d' % hits, end='', file=sys.stderr)
+            elif 'tags' in afapi.json:
+                print(' tags=%d' % len(afapi.json['tags']),
+                      end='', file=sys.stderr)
+
             if 'total' in afapi.json:
                 print(' total=%d' % afapi.json['total'],
                       end='', file=sys.stderr)
             elif 'hits' in afapi.json and 'total' in afapi.json['hits']:
                 print(' total=%d' % afapi.json['hits']['total'],
                       end='', file=sys.stderr)
+            elif 'total_count' in afapi.json:
+                print(' total_count=%d' % afapi.json['total_count'],
+                      end='', file=sys.stderr)
+
             if 'took' in afapi.json and afapi.json['took'] is not None:
                 d = datetime.timedelta(milliseconds=afapi.json['took'])
                 print(' time=%s' % d,
                       end='', file=sys.stderr)
+
             if 'af_message' in afapi.json:
                 print(' "%s"' % afapi.json['af_message'],
                       end='', file=sys.stderr)
