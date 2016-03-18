b43f7d0a31c30b494955b780f46e0e546a0bf098
Darlene Wong <dawong@paloaltonetworks.com>
2016-03-18 15:21:21 -0700

    Handle XML API for PanOS versions earlier than 4.1.0.

--- lib/pan/xapi.py
+++ lib/pan/xapi.py
@@ -74,7 +74,8 @@ class PanXapi:
                  use_http=False,
                  use_get=False,
                  timeout=None,
-                 ssl_context=None):
+                 ssl_context=None,
+                 legacy_api=False):
         self._log = logging.getLogger(__name__).log
         self.tag = tag
         self.api_username = None
@@ -179,7 +180,8 @@ class PanXapi:
         self.uri = '%s://%s' % (scheme, self.hostname)
         if self.port is not None:
             self.uri += ':%s' % self.port
-        self.uri += '/api/'
+        # legacy_api is used by panos < 4.1.0
+        self.uri += '/api/' if not legacy_api else '/esp/restapi.esp'
 
         if _legacy_urllib:
             self._log(DEBUG2, 'using legacy urllib')
