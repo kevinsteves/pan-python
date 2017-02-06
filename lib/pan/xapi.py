fb9fcc41cee7df62b8a7699103a41c7c523479b6
Kevin Steves <kevin.steves@pobox.com>
2017-02-05 19:22:35 -0700

    check for ssl.OPENSSL_VERSION since we allow 2.6.

--- lib/pan/xapi.py
+++ lib/pan/xapi.py
@@ -91,7 +91,8 @@ class PanXapi:
 
         self._log(DEBUG3, 'Python version: %s', sys.version)
         self._log(DEBUG3, 'xml.etree.ElementTree version: %s', etree.VERSION)
-        self._log(DEBUG3, 'ssl: %s', ssl.OPENSSL_VERSION)
+        if hasattr(ssl, 'OPENSSL_VERSION'):  # added 2.7
+            self._log(DEBUG3, 'ssl: %s', ssl.OPENSSL_VERSION)
         self._log(DEBUG3, 'pan-python version: %s', __version__)
 
         if self.port is not None:
